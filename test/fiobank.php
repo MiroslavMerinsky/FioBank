bd5b33f6f42c03582433001bae9728b6467a3a3f
MiroslavMerinsky <miroslav.merinsky@gmail.com>
2017-03-19 23:24:50 +0100

    Fix date in test file

--- test/fiobank.php
+++ test/fiobank.php
@@ -4,6 +4,6 @@ include_once '../src/FioBank/FioBank.php';
 include_once 'config.php';
 
 $bank = new \Merinsky\FioBank\FioBank(API_KEY);
-$bs = $bank->getBankStatement(\DateTime::createFromFormat('Y-m-d', '2017-01-01'), new \DateTime());
+$bs = $bank->getBankStatement(new \DateTime('2017-01-01'), new \DateTime());
 
 print_r($bs);
