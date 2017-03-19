ff23abcd694bdd2d83426184b740e8c8621444e8
MiroslavMerinsky <miroslav.merinsky@gmail.com>
2017-03-19 23:36:55 +0100

    Fix small bugs
    Check if mandatory items present in the statements.
    Rename hyphen in variable to underscore.
    Convert date to \DateTime object.

--- src/FioBank/FioBank.php
+++ src/FioBank/FioBank.php
@@ -44,25 +44,31 @@ class FioBank {
         $v = [];
 
         foreach ($data['accountStatement']['transactionList']['transaction'] as $item) {
+            if (!isset($item['column22']['value']) || !isset($item['column0']['value'])
+               || !isset($item['column1']['value']) || !isset($item['column14']['value'])
+               || !isset($item['column2']['value']) || !isset($item['column3']['value'])
+               || !isset($item['column8']['value']))
+               throw new \UnexpectedValueException('Mandatory items are missing in the statement.');
+                
             $v[] = [
-                'id' => isset($item['column22']['value']) ? $item['column22']['value'] : null,
-                'date' => isset($item['column0']['value']) ? $item['column0']['value'] : null,
-                'amount' =>  isset($item['column1']['value']) ? $item['column1']['value'] : null,
-                'currency' => isset($item['column14']['value']) ? $item['column14']['value'] : null,
-                'bank-account' => isset($item['column2']['value']) ? $item['column2']['value'] : null,
-                'bank-account-name' => isset($item['column10']['value']) ? $item['column10']['value'] : null,
-                'bank-code' => isset($item['column3']['value']) ? $item['column3']['value'] : null,
-                'bank-name' => isset($item['column12']['value']) ? $item['column12']['value'] : null,
+                'id' => $item['column22']['value'],
+                'date' => new \DateTime(substr($item['column0']['value'], 0, 10 /* date Y-m-d length*/)),
+                'amount' =>  $item['column1']['value'],
+                'currency' => $item['column14']['value'],
+                'bank_account' => $item['column2']['value'],
+                'bank_account_name' => isset($item['column10']['value']) ? $item['column10']['value'] : null,
+                'bank_code' => $item['column3']['value'],
+                'bank_name' => isset($item['column12']['value']) ? $item['column12']['value'] : null,
                 'ks' => isset($item['column4']['value']) ? $item['column4']['value'] : null,
                 'vs' => isset($item['column5']['value']) ? $item['column5']['value'] : null,
                 'ss' => isset($item['column6']['value']) ? $item['column6']['value'] : null,
                 'description' => isset($item['column7']['value']) ? $item['column7']['value'] : null,
                 'note' => isset($item['column16']['value']) ? $item['column16']['value'] : null,
-                'type' => isset($item['column8']['value']) ? $item['column8']['value'] : null,
+                'type' => $item['column8']['value'],
                 'person' => isset($item['column9']['value']) ? $item['column9']['value'] : null,
                 'comment' => isset($item['column25']['value']) ? $item['column25']['value'] : null,
-                'bank-dic' => isset($item['column26']['value']) ? $item['column26']['value'] : null,
-                'id-item' => isset($item['column17']['value']) ? $item['column17']['value'] : null,
+                'bank_dic' => isset($item['column26']['value']) ? $item['column26']['value'] : null,
+                'id_item' => isset($item['column17']['value']) ? $item['column17']['value'] : null,
             ];
         }
 
